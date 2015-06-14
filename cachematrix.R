be1c999eecb31eebe1e8fa31474074414e84f985
Alvaro Gutierrez <gutierrez.ec@gmail.com>
2015-06-14 17:34:45 -0500

    Subir cambios con comentarios

--- cachematrix.R
+++ cachematrix.R
@@ -1,48 +1,63 @@
-## Put comments here that give an overall description of what your
-## functions do
+#=======================================================================
+#cachematrix.R
+#Curso: Programacion en R
+#Objetivo: Almacenamiento en cache de la inversa de una matriz
+#Por: Alvaro Gutierrez		Fecha: 2015-06-14
+#=======================================================================
 
-## Write a short comment describing this function
+# Indicar la carpeta por default
+setwd("C:/Users/Alvaro/ProgrammingAssignment2")
 
-makeCacheMatrix <- function(x = matrix()) {
-        cache <- NULL
-        set <- function(y) {
-                x <<- y
-                cache <<- NULL
-        }
-        get <- function() x
-        setMatrix <- function(inverse) cache <<- inverse
-        getInverse <- function() cache
-        list(set = set, get = get, setMatrix = setMatrix, getInverse = getInverse)
+#=======================================================================
+#makeCacheMatrix Crea una matriz que puede ser usada para almacenamiento en cache
+#=======================================================================
+makeCacheMatrix <- function(x = matrix()) 
+{
+	#Almacena el valor del cache inicialmente a NULL
+	cache <- NULL
+	set <- function(y) 
+	{
+		x <<- y
+		cache <<- NULL
+	}
+	#Obtiene el valor de la matriz
+	get <- function() x
+	setMatrix <- function(inverse) cache <<- inverse
+	getInverse <- function() cache
+	#Retorna la funcion creada al environment de trabajo
+	list(set = set, get = get, setMatrix = setMatrix, getInverse = getInverse)
 }
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-        cache <- x$getInverse()
-        if (!is.null(cache)) {
-                message("getting cached data")
-                return(cache)
-        }
-        matrix <- x$get()
-        tryCatch( {
-                cache <- solve(matrix, ...)
-        },
-        error = function(e) {
-                message("Error:")
-                message(e)
-
-                return(NA)
-        },
-        warning = function(e) {
-                message("Warning:")
-                message(e)
-
-                return(NA)
-        },
-        finally = {
-                # set inverted matrix in cache
-                x$setMatrix(cache)
-        } )
-
-        # display matrix in console
-        return (cache)
+#=======================================================================
+#cacheSolve Obtiene o pone una matriz para almacenamiento en cache
+#=======================================================================
+cacheSolve <- function(x, ...) 
+{
+	#Trata de obtener la inversa
+	cache <- x$getInverse()
+	if (!is.null(cache)) {
+		message("getting cached data")
+		return(cache)
+	}
+	#Crea la matriz
+	matrix <- x$get()
+	#Asegurarse de que la matriz sea cuadrada e invertible
+	tryCatch( {
+		cache <- solve(matrix, ...)
+	},
+	error = function(e) {
+		message("Error:")
+		message(e)
+		return(NA)
+	},
+	warning = function(e) {
+		message("Cuidado:")
+		message(e)
+		return(NA)
+	},
+	finally = {
+		x$setMatrix(cache)
+	} )
+	#Desplegar matriz
+	return (cache)
 }
